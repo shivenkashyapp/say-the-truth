b993b44a1fff92e61b322f508b2f7478d992bf34
Naman Arora <namanarora1022@gmail.com>
2025-01-03 17:48:17 +0530

    feat: api connected

--- client/src/components/LLM.tsx
+++ client/src/components/LLM.tsx
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000';
 
 const LLM: React.FC = () => {
+    const [input, setInput] = useState('');
+    const [responses, setResponses] = useState<any>([]);
+
+    const handleSubmit = async () => {
+        try {
+            const res = await axios.post(API_URL, { input });
+            console.log(res.data);
+            setResponses(res.data);
+        } catch (error) {
+            console.error(error);
+        }
+
+        setInput('');
+    };
+
     return (
         <div className="flex flex-col justify-between">
             <h2>LLM</h2>
-            <div className="overflow-y-scroll">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Similique dignissimos quidem deserunt expedita vitae,
-                    blanditiis distinctio dolore amet aspernatur corporis,
-                    consequatur nostrum ipsum voluptatem neque rerum error
-                    laboriosam molestias aperiam.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Similique dignissimos quidem deserunt expedita vitae,
-                    blanditiis distinctio dolore amet aspernatur corporis,
-                    consequatur nostrum ipsum voluptatem neque rerum error
-                    laboriosam molestias aperiam.
-                </p>
+            <div className="overflow-y-scroll h-48">
+                {responses.map((res: any) => (
+                    <div key={res.title}>
+                        <h3>{res.title}</h3>
+                        <a href={res.href}>Link</a>
+                        <p>{res.body}</p>
+                    </div>
+                ))}
             </div>
-            <form>
-                <label htmlFor="prompt"></label>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
+                }}
+            >
+                <label htmlFor="prompt">Prompt</label>
                 <input
                     type="text"
                     name="prompt"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                     placeholder="What possible implications does this misinformation hold ?"
                     className="w-full rounded-md bg-[#949494] placeholder:text-gray-300 outline-none border-none p-2"
+                    autoComplete="off"
                 />
             </form>
         </div>
--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from inference.infer import infer
 from search.search import search_sources
+from flask_cors import CORS
 
 _source_list: list[str] = [
     "altnews.in",
@@ -11,14 +12,13 @@ _source_list: list[str] = [
 ] 
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000'])
 
-@app.route('', methods=['POST'])
+@app.route('/', methods=['POST'])
 def send_output():
     query: str = request.json.get('input', '')
-
-    output: str = infer(query, search_sources(query, _source_list))
-
-    return output[0]["response"]
+    # output: str = infer(query, search_sources(query, _source_list))
+    return [{'title': 'No, Viral Pic Doesn’t Show People Setting Liton Das’ House on Fire in ...', 'href': 'https://www.thequint.com/news/webqoof/liton-das-house-set-on-fire-bangladesh-viral-image-mashrafe-mortaza-fact-check', 'body': "Amidst the political turmoil in Bangladesh, a set of images is being shared on social media platforms with a claim that they show Bangladeshi cricketer Liton Das' house being recently set on fire ..."}, {'title': "Cricketer Liton Das' house set afire in Bangladesh? No, viral image ...", 'href': 'https://www.altnews.in/cricketer-liton-das-house-set-afire-in-bangladesh-no-viral-image-shows-narail-residence-of-mashrafe-mortaza/', 'body': 'Therefore, the claim that Liton Das’ house has been set on fire and that the viral image showed Das’ house is false. The viral image showed the house of former cricketer and Awami League MP Mashrafe Bin Mortaza’s house. Donate to Alt News! Independent journalism that speaks truth to power and is free of corporate and political control is possible only when people start contributing towards the same. Please consider donating towards this endeavour to fight fake news and misinformation.'}]
 
 if __name__ == '__main__':
     app.run(debug = True)
